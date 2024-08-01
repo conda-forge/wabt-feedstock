5517f58c016883ed8d002de90350f96ef3e85db6
jakirkham <jakirkham@gmail.com>
2024-07-31 23:26:34 -0700

    Add missing errorlevel checks on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,11 @@
 set CMAKE_CONFIG=Release
 
 mkdir build_%CMAKE_CONFIG%
+if errorlevel 1 exit 1
+
 pushd build_%CMAKE_CONFIG%
+if errorlevel 1 exit 1
+
 cmake -G "Ninja" ^
     %CMAKE_ARGS% ^
     -DCMAKE_BUILD_TYPE:STRING="%CMAKE_CONFIG%" ^
@@ -10,5 +14,10 @@ cmake -G "Ninja" ^
     -DBUILD_SHARED_LIBS:BOOL=ON ^
     -DBUILD_TESTS:BOOL=OFF ^
     "%SRC_DIR%"
+if errorlevel 1 exit 1
+
 cmake --build . --target install --config %CMAKE_CONFIG%
+if errorlevel 1 exit 1
+
 popd
+if errorlevel 1 exit 1
